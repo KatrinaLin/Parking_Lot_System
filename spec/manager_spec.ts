2f365b43a6309b1531f4162354253f912476c8bc
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-10 23:35:51 +0800

    feature: implement manager park

--- spec/manager_spec.ts
+++ spec/manager_spec.ts
@@ -1,5 +1,6 @@
 import "jasmine"
 import {Manager} from "../src/manager";
+import ParkingLot from "../src/parking_lot";
 
 describe("manager", () => {
 
@@ -11,5 +12,18 @@ describe("manager", () => {
 
     it("should have field id", () => {
         expect(manager.id).toBe("David");
+    });
+
+    it("should park car to first available parking lot", () => {
+        const pl1 = new ParkingLot(5);
+        const pl2 = new ParkingLot(5);
+
+        manager.assignParkingLot(pl1);
+        manager.assignParkingLot(pl2);
+
+        manager.park();
+
+        expect(pl1.occupied).toBe(1);
+        expect(pl2.occupied).toBe(0);
     });
 })
