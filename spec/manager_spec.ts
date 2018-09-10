e635bd8edae907f90e3dfcea43a37cc1bfad1b2f
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-10 23:40:03 +0800

    feature: implement manager assign parking boy

--- spec/manager_spec.ts
+++ spec/manager_spec.ts
@@ -1,6 +1,9 @@
 import "jasmine"
 import {Manager} from "../src/manager";
 import ParkingLot from "../src/parking_lot";
+import {ParkingBoy} from "../src/parking_boy";
+import {SmartParkingBoy} from "../src/smart_parking_boy";
+import {SuperParkingBoy} from "../src/super_parking_boy";
 
 describe("manager", () => {
 
@@ -25,5 +28,19 @@ describe("manager", () => {
 
         expect(pl1.occupied).toBe(1);
         expect(pl2.occupied).toBe(0);
+    });
+
+    it("should manage a list of parking boys", () => {
+        const pb1 = new ParkingBoy("PBTom");
+        const pb2 = new SmartParkingBoy("SmartKavin");
+        const pb3 = new SuperParkingBoy("SuperMario");
+
+        manager.assignParkingBoy(pb1);
+        manager.assignParkingBoy(pb2);
+        manager.assignParkingBoy(pb3);
+
+        expect(manager.parkingBoyList.indexOf(pb1)).toBeGreaterThan(-1);
+        expect(manager.parkingBoyList.indexOf(pb2)).toBeGreaterThan(-1);
+        expect(manager.parkingBoyList.indexOf(pb3)).toBeGreaterThan(-1);
     });
 })
