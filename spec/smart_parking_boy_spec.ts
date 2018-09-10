0227e83e9c9b4f5a28d5bb6205399524ab652e37
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-10 23:08:28 +0800

    feature: implement smart parking boy

--- spec/smart_parking_boy_spec.ts
+++ spec/smart_parking_boy_spec.ts
@@ -1,17 +1,36 @@
 import "jasmine";
-import {Smart_parking_boy} from "../src/smart_parking_boy";
+import {SmartParkingBoy} from "../src/smart_parking_boy";
+import ParkingLot from "../src/parking_lot";
 
 describe("parking_boy", () => {
 
-    let smartPB: Smart_parking_boy;
+    let smartPB: SmartParkingBoy;
 
     beforeEach(() => {
-        smartPB = new Smart_parking_boy("Tom");
+        smartPB = new SmartParkingBoy("Tom");
     });
 
     it("should have field id", () => {
         expect(smartPB.id).toBe("Tom");
     });
 
+    it("should park car to parking lot with the most available spots", () => {
+        const pl1 = new ParkingLot(5);
+        const pl2 = new ParkingLot(5);
+        const pl3 = new ParkingLot(5);
 
+        pl1.occupied = 3;
+        pl2.occupied = 1;
+        pl3.occupied = 4;
+
+        smartPB.assignParkingLot(pl1);
+        smartPB.assignParkingLot(pl2);
+        smartPB.assignParkingLot(pl3);
+
+        smartPB.park();
+
+        expect(pl1.occupied).toBe(3);
+        expect(pl2.occupied).toBe(2);
+        expect(pl3.occupied).toBe(4);
+    });
 })
--- src/smart_parking_boy.ts
+++ src/smart_parking_boy.ts
@@ -1,6 +1,14 @@
 import {ParkingBoy} from "./parking_boy";
 
-export class Smart_parking_boy extends ParkingBoy{
+export class SmartParkingBoy extends ParkingBoy{
 
+    park(): string {
+        this.parkingLotList.sort((a, b) => a.occupied - b.occupied);
+        let pl = this.parkingLotList.filter(pl => pl.occupied < pl.capacity);
+        if (pl.length == 0) {
+            return "Sorry, all my parking lots are full."
+        }
+        return pl[0].addCar();
+    }
 
 }
